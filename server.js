c9c64a3db436b01dd41beaa9761cb99fbee10bf5
Cristy Duran <duran_cris43@yahoo.com>
2023-10-17 22:03:56 -0700

    set up validation

--- server.js
+++ server.js
@@ -9,4 +9,26 @@ app.use((req, res, next) => {
 })
 app.use(express.json({limit: '10mb'}))
 
+let db = new sqlite3.Database('credentials.db', (err) => {
+    if (err) {
+        console.log(err.message);
+    }
+    console.log('Connected to the access database.')
+})
+
+app.post('/validatePassword', (req, res) => {
+    const { username, password } = req.body
+
+    db.all(`SELECT * FROM credentials WHERE username = '${username}' AND password = '${password}'`, (err, rows) => {
+        if(err) {
+            throw err;
+        }
+        if(rows.length > 0) {
+            res.send({validation: true})
+        } else {
+            res.send({validation: false})
+        }
+    })
+})
+
 app.listen(3001, () => console.log('Listening at port 3001'))
